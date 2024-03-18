bbe8ebdfb9e2d1f8b17ac6f10d24c516719b3c29
mikemwai <mwai.michael@strathmore.edu>
2024-03-18 09:36:13 +0300

    API added

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect} from 'react';
 import { copy, linkIcon, loader, tick } from '../assets';
+import { useLazyGetSummaryQuery } from '../services/article';
 
 const Demo = () => {
     const [article, setArticle] = useState({
@@ -8,23 +9,31 @@ const Demo = () => {
         summary: '',
     });
 
+    const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
     const handleSubmit = async (e) => {
-        alert('Submitted');
+        e.preventDefault();
+        const { data } = await getSummary({ articleUrl: article.url });
+        if (data?.summary) {
+            const newArticle = { ...article, summary: data.summary };
+            setArticle(newArticle);
+            console.log(newArticle);
+        }
     }
 
     return (
-        <section className="mt-16 w-full max-w-xl">
-            <div className="flex flex-col w-full gap-2">
-                <form action="" className="relative flex justify-center items-center" onSubmit={handleSubmit}>
-                    <img src={linkIcon} alt='link_icon' className="absolute left-0 my-2 ml-3 w-5" />
+        <section className='mt-16 w-full max-w-xl'>
+            <div className='flex flex-col w-full gap-2'>
+                <form className='relative flex justify-center items-center' onSubmit={handleSubmit}>
+                    <img src={linkIcon} alt='link_icon' className='absolute left-0 my-2 ml-3 w-5' />
                     <input 
                         type="url" 
                         placeholder="Enter a URL" 
                         value={article.url} 
                         onChange={(e) => setArticle({...article, url: e.target.value})} 
-                        required className="url_input peer" 
+                        required className='url_input peer'
                     />
-                    <button type="submit" className="submit_btn">
+                    <button type="submit" className='submit_btn'>
                             <img src={tick} alt='submit_icon peer-focus:border-gray-700 peer-focus:text-gray-700' />  
                     </button>
                 </form>
