e905221ca35186d8e665999e73ea8082adb87a72
mikemwai <mwai.michael@strathmore.edu>
2024-03-18 12:32:35 +0300

    Display Section Added

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect} from 'react';
 import { copy, linkIcon, loader, tick } from '../assets';
@@ -65,7 +66,26 @@ const Demo = () => {
                 </div>
             </div>
 
-
+            <div className="my-10 max-w-full flex justify-center items-center">
+                {isFetching ? (
+                    <img src={loader} alt='loader' className="w-20 h-20 object-contain" />
+                ) : error ? (
+                    <p className='font-inter font-bold text-black text-center'>
+                        Well that wasn't supposed to happen...
+                        <br />
+                        <span className="font-satoshi font-normal text-gray-700">{error?.data?.error}</span>
+                    </p>
+                ) : (
+                    article.summary && (
+                        <div className="flex flex-col gap-3">
+                            <h2 className="font-satoshi font-bold text-gray-600 text-xl">Article <span className="bg-clip-text text-transparent bg-blue-gradient">Summary</span></h2>
+                            <div className="summary_box">
+                                <p className="font-inter font-medium text-sm text-gray-700">{article.summary}</p>
+                            </div>
+                        </div>
+                    )
+                )}
+            </div>
         </section>
     );
 }
