e11a7fe14fad2167f5606075f1c491477aa74411
mikemwai <mwai.michael@strathmore.edu>
2024-03-18 11:44:14 +0300

    Browser URL history

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -9,15 +9,26 @@ const Demo = () => {
         summary: '',
     });
 
+    const [allArticles, setAllArticles] = useState([]);
+
     const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+    useEffect(() => {
+        const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'));
+        if (articlesFromLocalStorage) {
+            setAllArticles(articlesFromLocalStorage);
+        }
+    }, []);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         const { data } = await getSummary({ articleUrl: article.url });
         if (data?.summary) {
             const newArticle = { ...article, summary: data.summary };
+            const updatedAllArticles = [newArticle, ...allArticles];
             setArticle(newArticle);
-            console.log(newArticle);
+            setAllArticles(updatedAllArticles);
+            localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
         }
     }
 
@@ -37,6 +48,21 @@ const Demo = () => {
                             <img src={tick} alt='submit_icon peer-focus:border-gray-700 peer-focus:text-gray-700' />  
                     </button>
                 </form>
+                
+                    <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+                    {allArticles.map((item, index) => (
+                        <div 
+                        key={`link-${index}`}
+                        onClick={() => setArticle(item)}
+                        className='link_card'
+                        >
+                            <div className="copy_btn">
+                                <img src={copy} alt="copy_icon" className="w-[40%] h-[40%] object-contain" />
+                            </div>
+                            <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">{item.url}</p>
+                        </div>
+                    ))}
+                </div>
             </div>
 
 
